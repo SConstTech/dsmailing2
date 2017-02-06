40e63ac1bbe2a3bd2a95b506fd48ca5860f0bc48
Stoyan Konstantinov <stoyan.konstantinov@gmail.com>
2017-02-07 00:04:49 +0200

    Added some templating

--- dsmailing/urls.py
+++ dsmailing/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
+    url(r'^login/$', TemplateView.as_view(template_name='login.html')),
 ]
