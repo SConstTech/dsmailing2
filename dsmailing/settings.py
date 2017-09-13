0e95caa8f2225028b6deb84fa6b70f2f9c313a49
Stoyan Konstantinov <stoyan.konstantinov@gmail.com>
2017-09-13 08:51:57 +0300

    added some changes in settings

--- dsmailing/settings.py
+++ dsmailing/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'dsmailing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dsmailingdb',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
