d17be9927a1e575c8783f56c37570cd38adb827e
Stoyan Konstantinov <stoyan.konstantinov@gmail.com>
2017-09-14 08:16:15 +0300

    Fixed logout redirect global URL

--- dsmailing/settings.py
+++ dsmailing/settings.py
@@ -124,6 +124,8 @@ USE_L10N = True
 USE_TZ = True
 
 LOGIN_REDIRECT_URL = '/system'
+LOGIN_URL = '/system/login'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
